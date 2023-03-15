9ff8f3d2bc4bb95a83d97cdc5320951dff7d51e3
pavel <pospelov2013@mail.ru>
2023-03-15 23:10:51 +0500

    Initial commit

---
+++ src/main/java/ru/students/testrestservice/TestRestServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.students.testrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRestServiceApplication.class, args);
+	}
+
+}
